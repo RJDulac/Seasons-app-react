723f14fba837aece58ca56ab0109dec798ba4fe6
Ryan Dulac <analyticrye@gmail.com>
2019-01-27 01:09:20 -0500

    seasons added

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,38 @@
 import React from "react";
+import "./SeasonDisplay.css";
+//design pattern to make clearer code
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr it is cold!",
+    iconName: "snowflake"
+  }
+};
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  // {text, iconName} contained inside seasonConfig[season]
+  //destructure
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    //root element should have a class name the same as the component name. in this case season-display
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -3,26 +3,29 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    //we are overriding constructor function
-    //super is a reference to the parent's constructor function. Super calls the parent's or React.Component's constructor function. Need to initialize constructor function
-    super(props);
-    //only time we directly assign state
-    this.state = {
-      //property/value
-      lat: null,
-      errorMessage: ""
-    };
+  // constructor(props) {
+  //   //we are overriding constructor function
+  //   //super is a reference to the parent's constructor function. Super calls the parent's or React.Component's constructor function. Need to initialize constructor function
+  //   super(props);
+  //   //only time we directly assign state
+  //   this.state = {
+  //     //property/value
+  //     lat: null,
+  //     errorMessage: ""
+  //   };
+  // }
+  //babel builds the contructor for us.
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       //first callback -success callback
-      position => {
-        //never directly assign setState - only when initializing
-        this.setState({ lat: position.coords.latitude });
-      },
+      position => this.setState({ lat: position.coords.latitude }),
+      //never directly assign setState - only when initializing
       //second callback -when failed to find location
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      err => this.setState({ errorMessage: err.message })
     );
   }
   render() {
@@ -32,7 +35,8 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      //passing state in a prop
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading!</div>;
   }
